6ae8b0c0cd93cad491d19b8da0147a43c7fd7dc0
Naoyuki Ishida <39.ishida@gmail.com>
2023-06-06 16:07:59 -0700

    Avatarの変更のテストを追加

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,7 +1,11 @@
 module UserHelper
   def avatar
-    unless current_user.avatar.nil?
-      image_tag current_user.avatar.url, alt: 'mdo', size: '32x32', class: 'rounded-circle'
+    if user_signed_in?
+      if current_user.avatar.present?
+        image_tag current_user.avatar.url, alt: 'mdo', size: '32x32', class: 'rounded-circle'
+      else
+        image_tag 'user.png', alt: 'mdo', size: '32x32', class: 'rounded-circle'
+      end
     else
       image_tag 'user.png', alt: 'mdo', size: '32x32', class: 'rounded-circle'
     end
