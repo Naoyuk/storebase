8c5158b00cc1c3cc209f84c81c5a46dfe417a9c7
Naoyuki Ishida <39.ishida@gmail.com>
2023-06-27 22:58:04 -0700

    Navbarのドロップダウンメニューが動かなかったバグを修正
    convert.jsで、/converter以外のページでイベントリスナーの対象オブジェクトが見つからずクラッシュしていたのが原因。
    URLでJavaScriptが動くかどうかを分岐して対応。
    また、Turboが原因でページ表示直後にJavaScriptが動かず、リロードを必要としていたバグもあり、これも修正。

--- app/javascript/convert.js
+++ app/javascript/convert.js
@@ -1,5 +1,12 @@
-document.getElementById('feature_id').addEventListener('change', function() {
-  const selectedFeatureId = this.value;
-  const form = document.getElementById('csv_convert_form');
-  form.action = `/features/${selectedFeatureId}/convert`;
+document.addEventListener('turbo:load', function() {
+  if (window.location.pathname === '/converter') {
+    let featureElement = document.getElementById('feature_id');
+    if (featureElement != null) {
+      featureElement.addEventListener('change', function() {
+        const selectedFeatureId = this.value;
+        const form = document.getElementById('csv_convert_form');
+        form.action = `/features/${selectedFeatureId}/convert`;
+      });
+    }
+  }
 });
