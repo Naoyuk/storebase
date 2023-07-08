d5e805ebc3562adc7037fca755ee579b1d25328b
Naoyuki Ishida <39.ishida@gmail.com>
2023-07-07 17:47:33 -0700

    Add faker on user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "test#{n}@example.com" }
+    email { Faker::Internet.unique.email }
     sequence(:user_name) { |n| "test#{n}" }
     sequence(:first_name) { |n| "test#{n}first" }
     sequence(:last_name) { |n| "test#{n}last" }
